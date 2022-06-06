c1e99ed747b36c8aba4f6ba944f334ba8811ce8f
Matija Pevec <pevec@live.com>
2022-06-06 18:13:55 +0200

    update to latest uniswap version

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import './App.css'
 import {useCallback, useEffect, useState} from 'react'
-import {SwapWidget} from '@uniswap/widgets'
+import {SwapWidget, SwapWidgetProps} from '@uniswap/widgets'
 import {IFrameEthereumProvider} from '@ethvault/iframe-provider'
-import {Provider} from '@web3-react/types'
-import {TokenInfo} from '@uniswap/token-lists'
-import {providers, Signer} from 'ethers'
+import {providers} from 'ethers'
 
 function App() {
     const [widgetConfig, setWidgetConfig] = useState({} as Partial<SwapWidgetProps>)
@@ -36,32 +34,26 @@ function App() {
     const iframeProvider = new IFrameEthereumProvider() as any
     iframeProvider.request = iframeProvider.send
 
-    // TODO: uncomment when error on widget side is resolved
-    // Swap failed Error: invalid object key - gas (argument="transaction:gas", value={"gas":"0x26bbe","value":"0xdbf4a8777c6c248","from":"0x9a72ad187229e9338c7f21e019544947fb25d473","to":"0x68b3465833fb72a70ecdf485e0e4c7bd8665fc45","data":"0x5ae401dc000000000000000000000000000000000000000000000000000000006213917500000000000000000000000000000000000000000000000000000000000000400000000000000000000000000000000000000000000000000000000000000001000000000000000000000000000000000000000000000000000000000000002000000000000000000000000000000000000000000000000000000000000000e404e45aaf0000000000000000000000000d500b1d8e8ef31e21c99d1db9a6444d3adf12700000000000000000000000007ceb23fd6bc0add59e62ac25578270cff1b9f6190000000000000000000000000000000000000000000000000000000000000bb80000000000000000000000009a72ad187229e9338c7f21e019544947fb25d4730000000000000000000000000000000000000000000000000dbf4a8777c6c2480000000000000000000000000000000000000000000000000001ff23625df5c3000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000"}, code=INVALID_ARGUMENT, version=properties/5.5.0)
-    //
-    // const signer = new providers.Web3Provider(iframeProvider).getSigner()
-    // const provider = new providers.StaticJsonRpcProvider(widgetConfig.jsonRpcEndpoint!)
-    //
-    // const widgetProvider = {
-    //     signer: signer,
-    //     provider: provider
-    // }
-    const widgetProvider = iframeProvider
+    const signer = new providers.Web3Provider(iframeProvider).getSigner()
+    const provider = (typeof widgetConfig.jsonRpcEndpoint === 'string') ?
+        new providers.StaticJsonRpcProvider(widgetConfig.jsonRpcEndpoint! as string) :
+        widgetConfig.jsonRpcEndpoint
 
     return (
         <SwapWidget
             theme={widgetConfig.theme}
             locale={widgetConfig.locale}
-            provider={widgetProvider}
+            provider={signer.provider}
+            jsonRpcEndpoint={provider}
             width={widgetConfig.width}
             dialog={widgetConfig.dialog}
             className={widgetConfig.className}
             onError={(error, info) => window.parent.postMessage(outputMessage('onError', {error, info}), '*')}
 
             tokenList={widgetConfig.tokenList}
-            defaultInputAddress={widgetConfig.defaultInputAddress}
+            defaultInputTokenAddress={widgetConfig.defaultInputTokenAddress}
             defaultInputAmount={widgetConfig.defaultInputAmount}
-            defaultOutputAddress={widgetConfig.defaultOutputAddress}
+            defaultOutputTokenAddress={widgetConfig.defaultInputTokenAddress}
             defaultOutputAmount={widgetConfig.defaultOutputAmount}
             convenienceFee={widgetConfig.convenienceFee}
             convenienceFeeRecipient={widgetConfig.convenienceFeeRecipient}
@@ -92,72 +84,5 @@ interface WidgetEventMessageOutputData {
 
 type WidgetInputMethod = 'reload' | 'setConfig'
 type WidgetOutputMethod = 'onConnectWallet' | 'onError'
-
-// Widget specific types
-
-type SwapWidgetProps = SwapProps & WidgetProps
-
-interface SwapProps {
-    tokenList?: string | TokenInfo[];
-    defaultInputAddress?: DefaultAddress;
-    defaultInputAmount?: string;
-    defaultOutputAddress?: DefaultAddress;
-    defaultOutputAmount?: string;
-    convenienceFee?: number;
-    convenienceFeeRecipient?: string | {
-        [chainId: number]: string;
-    };
-    onConnectWallet?: () => void;
-}
-
-type DefaultAddress = string | {
-    [chainId: number]: string | 'NATIVE';
-} | 'NATIVE';
-
-interface WidgetProps {
-    theme?: Theme;
-    locale?: string;
-    provider?: Provider | providers.Provider | ProviderWithSigner;
-    jsonRpcEndpoint?: string;
-    width?: string | number;
-    dialog?: HTMLElement | null;
-    className?: string;
-    onError?: (error: any, info: any) => void;
-}
-
-interface ProviderWithSigner {
-    provider: providers.Provider;
-    signer: Signer;
-}
-
-interface Theme extends Partial<Attributes>, Partial<Colors> {
-}
-
-interface Attributes {
-    borderRadius: boolean | number;
-    fontFamily: string;
-    fontFamilyVariable: string;
-    fontFamilyCode: string;
-    tokenColorExtraction: boolean;
-}
-
-interface Colors {
-    accent: string;
-    container: string;
-    module: string;
-    interactive: string;
-    outline: string;
-    dialog: string;
-    primary: string;
-    onAccent: string;
-    secondary: string;
-    hint: string;
-    onInteractive: string;
-    active: string;
-    success: string;
-    warning: string;
-    error: string;
-    currentColor: 'currentColor';
-}
 
 export default App
